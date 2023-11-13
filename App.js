dcc8f3654f2bbe1aefff131d4c22e7e160978588
HugoC <hugicash@gmail.com>
2023-11-13 10:54:57 +0100

    app

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import CreateUserScreen from "./screens/CreateUserScreen";
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import Futbol from './screens/Futbol';
+import Baloncesto from './screens/Baloncesto';
+import Tenis from './screens/Tenis';
+import CarrerasCaballos from './screens/CarrerasCaballos';
 
-const Stack=createNativeStackNavigator();
-
-function MyStack(){
-  return(
-        <Stack.Navigator>
-          <Stack.Screen name="CreateUserScreen" component={CreateUserScreen}/>
-          
-        </Stack.Navigator>
-  )
-}
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer >
-      <MyStack></MyStack>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Futbol" component={Futbol} />
+        <Stack.Screen name="Baloncesto" component={Baloncesto} />
+        <Stack.Screen name="Tenis" component={Tenis} />
+        <Stack.Screen name="CarrerasCaballos" component={CarrerasCaballos} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
 
